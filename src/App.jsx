332b8e521b393309b548a1389ab806d6d7b5fe3a
kartikgame369 <kartikgame369doc@gmail.com>
2025-12-15 07:49:28 +0530

    Initial Vite + Tailwind project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <h1 className=' bg-red-500'>hello check every thing okay </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
