c83321c7178dc3aab35d61714955726af3a17e5b
kartikgame369 <kartikgame369doc@gmail.com>
2025-12-17 12:27:48 +0530

    Add name validation

--- src/utils/Validate.jsx
+++ src/utils/Validate.jsx
@@ -1,8 +1,8 @@
 
-const checkvalidData = (email , password)=>{
+const checkvalidData = (email , password ,name)=>{
     const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
     const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
-    const isNameValid = true; 
+    const isNameValid = name ? name.length >= 8 : true;
     if(!isEmailValid){
         return "Invalid email format";
     }
@@ -10,7 +10,7 @@ const checkvalidData = (email , password)=>{
         return "Password must be at least 8 characters long";
     }
     if(!isNameValid){
-        return "Password must be at least 8 characters long";
+        return "enter correct username";
     }
     return null;
 }
