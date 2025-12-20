b6f7455e04c9f84b034beb6bbeb178529c07e6f2
kartikgame369 <kartikgame369doc@gmail.com>
2025-12-20 16:23:16 +0530

    Add Redux store and user slice for state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './App.css'
 import Body from './components/Body.jsx'
-
+import appStore from './utils/appStore.jsx'
+import { Provider } from "react-redux";
 function App() {
   
   return (
+  <Provider store={appStore}>
     <Body/>
+  </Provider>
   )
 }
 
